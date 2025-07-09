30afd55b2cf7e7611e97f2d267eafc1520b2e23f
Farman-Ali-1 <107276852+Farman-Ali-1@users.noreply.github.com>
2025-07-09 23:03:12 +0500

    taking pull of detail page

--- src/app/(pages)/shop_all/page.tsx
+++ src/app/(pages)/shop_all/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function page() {
   return (
-    <div>shope all</div>
-  )
+    <div className="flex flex-row w-full gap-2">
+      <div className="flex flex-col gap-2 w-1/3 p-2">
+        <h1 className="text-[#CBA135] text-4xl font-bold">.AZURA</h1>
+        <h1 className="text-[#CBA135] text-3xl ">Conscious Reads for the Modern Minimalist.</h1>
+      </div>
+      <div className="flex flex-col gap-2 w-2/3 items-start">
+        <p className="text-[#CBA135] ">
+          Welcome to the Azura,redefines indulgence with premium Medjool dates,
+          blending desert heritage and luxurious rituals. Each curated creation
+          embodies artisanal craftsmanship, exclusivity, and timeless elegance —
+          a sophisticated statement for discerning, experience-driven clientele.
+        </p>
+        <div className="flex flex-row justify-center items-center">
+          <span className="px-4 py-1 w-fit rounded-2xl bg-white text-gray-600">
+            All
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default page
+export default page;
